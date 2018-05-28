011b7da21edc92defdf317d519865fb442a3d3e3
Daniel-Mack <daniel.mack@powerhrg.com>
2018-05-28 15:03:44 -0400

    Completed lab

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,31 @@
 class BankAccount
 
+  attr_accessor :balance, :status
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+
+  def deposit(bank_account)
+    @balance += @balance
+  end
+
+  def display_balance
+    return "Your balance is $#{balance}."
+  end
+
+  def valid?
+    @status == "open" && @balance > 0
+  end
+
+  def close_account
+    if @bank_account == valid?
+      true
+    else
+      self.status = "closed"
+    end
+  end
 end
